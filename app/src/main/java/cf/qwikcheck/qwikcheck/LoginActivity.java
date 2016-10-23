c394cb698b0eff52763ac9ee691e61482e9e891e
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-10-24 03:18:56 +0530

    Added login() in LoginActivity Added Volley Library

--- app/src/main/java/cf/qwikcheck/qwikcheck/LoginActivity.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/LoginActivity.java
@@ -1,9 +1,27 @@
 package cf.qwikcheck.qwikcheck;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import cf.qwikcheck.qwikcheck.Base.QwikCheckBaseActivity;
+import cf.qwikcheck.qwikcheck.Utils.Constants;
 
 public class LoginActivity extends QwikCheckBaseActivity {
 
@@ -11,5 +29,65 @@ public class LoginActivity extends QwikCheckBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+
+        // Login Button
+        Button login_button = (Button) findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(username.getText().toString().trim(),password.getText().toString().trim());
+            }
+        });
+
+        // Sign up Button
+        Button signup_button = (Button) findViewById(R.id.signup_button);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    public void login(final String username,final String password) {
+
+        final ProgressDialog dialog = ProgressDialog.show(this, "Logging in", "Please wait...", true);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, Constants.USER_LOGIN_URL,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Error.Response", response);
+                        dialog.dismiss();
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+    }
+
 }
--- app/src/main/java/cf/qwikcheck/qwikcheck/Utils/Constants.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/Utils/Constants.java
@@ -7,6 +7,7 @@ package cf.qwikcheck.qwikcheck.Utils;
 public class Constants {
 
     public static final String API_BASE_URL = "http://qwikcheck.cf/";
+    public static final String USER_LOGIN_URL = API_BASE_URL + "users/login.php";
 
     // Preferences keys
     public static final String PREF_FILE_NAME = "QWIKCHECK_PREF_FILE";
