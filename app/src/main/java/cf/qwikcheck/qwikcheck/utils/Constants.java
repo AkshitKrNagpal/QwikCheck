8c65ea0eb9427f97910e717c2d657db5d273a003
akshitkrnagpal <nagpalkrakshit@gmail.com>
2016-10-23 18:55:00 +0530

    Added SessionHelper Class and Contants class

---
+++ app/src/main/java/cf/qwikcheck/qwikcheck/utils/Constants.java
@@ -0,0 +1,16 @@
+package cf.qwikcheck.qwikcheck.utils;
+
+/**
+ * Created by akshit on 23/10/16.
+ */
+
+public class Constants {
+
+    public static final String API_BASE_URL = "http://qwikcheck.cf/";
+
+
+    // Preferences keys
+    public static final String USER_ID_KEY = "userID";
+    public static final String USER_NAME_KEY = "userName";
+    public static final String REAL_NAME_KEY = "realName";
+ }
