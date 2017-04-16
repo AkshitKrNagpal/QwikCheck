8e9a424d40ec72b31ef307dced4ddcf4de4cef5c
akshitkrnagpal <akshitkrnagpal@gmail.com>
2017-04-17 05:13:30 +0530

    Completed ViewProfile Activity

--- app/src/main/java/cf/qwikcheck/qwikcheck/ViewProfileActivity.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/ViewProfileActivity.java
@@ -1,40 +1,33 @@
 package cf.qwikcheck.qwikcheck;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
+import cf.qwikcheck.qwikcheck.base.QwikCheckBaseActivity;
 import cf.qwikcheck.qwikcheck.helper.SessionHelper;
-import cf.qwikcheck.qwikcheck.utils.Constants;
 
-public class ViewProfileActivity extends Activity {
+public class ViewProfileActivity extends QwikCheckBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if( toolbar != null ) {
+            setSupportActionBar(toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("View Profile");
+            }
+        }
+
         final TextView fullname = (TextView) findViewById(R.id.fullname);
         final TextView address = (TextView) findViewById(R.id.address);
         final TextView contact_no = (TextView) findViewById(R.id.contact_no);
-        final TextView type = (TextView) findViewById(R.id.type);
+        //final TextView type = (TextView) findViewById(R.id.type);
 
         SessionHelper sessionHelper = new SessionHelper(this);
 
@@ -47,6 +40,6 @@ public class ViewProfileActivity extends Activity {
             contact_no.setText(sessionHelper.getContactNo());
         else
             contact_no.setText("NULL");
-        type.setText(sessionHelper.getUsertype());
+        //type.setText(sessionHelper.getUsertype());
     }
 }
