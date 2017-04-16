96cf2f530063eeeafe4b73f7a5a8760359cac479
akshitkrnagpal <akshitkrnagpal@gmail.com>
2017-04-17 02:29:45 +0530

    Completed ViewMyVehicles activity

--- app/src/main/java/cf/qwikcheck/qwikcheck/ViewMyVehicleActivity.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/ViewMyVehicleActivity.java
@@ -5,7 +5,12 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -15,6 +20,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,19 +28,27 @@ import java.awt.font.TextAttribute;
 import java.util.HashMap;
 import java.util.Map;
 
+import cf.qwikcheck.qwikcheck.base.QwikCheckBaseActivity;
 import cf.qwikcheck.qwikcheck.helper.SessionHelper;
 import cf.qwikcheck.qwikcheck.utils.Constants;
 
-public class ViewMyVehicleActivity extends Activity {
+public class ViewMyVehicleActivity extends QwikCheckBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_my_vehicle);
 
-        final ProgressDialog LoadingDialog = ProgressDialog.show(this, "Loading", "Please wait...", true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if( toolbar != null ) {
+            setSupportActionBar(toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("My Vehicles");
+            }
+        }
 
-        final TextView my_vehicle_1 = (TextView) findViewById(R.id.my_vehicle_1);
+        final ProgressDialog LoadingDialog = ProgressDialog.show(this, "Loading", "Please wait...", true);
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
@@ -65,9 +79,18 @@ public class ViewMyVehicleActivity extends Activity {
                                         .show();
                             } else {
 
-                                if( jsonObject.getInt("count") == 0 ) {
-                                    String vehicle_number = jsonObject.getString("message");
-                                    my_vehicle_1.setText(vehicle_number);
+                                if( jsonObject.getInt("count") > 0 ) {
+
+                                    JSONArray vehicles = jsonObject.getJSONArray("vehicles");
+
+                                    for( int i=0; i < vehicles.length(); i++) {
+
+                                        JSONObject vehicle = vehicles.getJSONObject(i);
+
+                                        addVehicleToContainer(vehicle);
+
+                                    }
+
                                 }
 
                             }
@@ -108,7 +131,46 @@ public class ViewMyVehicleActivity extends Activity {
         };
         queue.add(postRequest);
 
+        container = (LinearLayout) findViewById(R.id.my_vehicle_list_container);
 
+    }
+
+    LinearLayout container;
+
+    public void addVehicleToContainer(JSONObject challan) {
+
+        LinearLayout.LayoutParams rootParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        rootParams.setMargins(32, 32, 32, 32);
+
+        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        llParams.setMargins(8, 8, 8, 8);
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        CardView root = new CardView(this);
+        root.setRadius(16);
+        root.setLayoutParams(rootParams);
+        //root.setPadding(64,64,64,64);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(llParams);
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setPadding(32, 16, 32, 16);
+
+        TextView regno = new TextView(this);
+        regno.setLayoutParams(layoutParams);
+        regno.setTextSize(24);
+        try {
+            regno.setText(challan.getString("RegNo"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        linearLayout.addView(regno);
+
+        root.addView(linearLayout);
+
+        container.addView(root);
 
     }
 }
