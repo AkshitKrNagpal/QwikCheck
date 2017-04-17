4c3cf16d5933bdf397a78e39d5f5ffe2e32f766a
akshitkrnagpal <akshitkrnagpal@gmail.com>
2017-04-17 06:37:35 +0530

    Completed EnterRegNoActivity

--- app/src/main/java/cf/qwikcheck/qwikcheck/EnterRegNoActivity.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/EnterRegNoActivity.java
@@ -3,18 +3,33 @@ package cf.qwikcheck.qwikcheck;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class EnterRegNoActivity extends Activity {
+import cf.qwikcheck.qwikcheck.base.QwikCheckBaseActivity;
+
+public class EnterRegNoActivity extends QwikCheckBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_reg_no);
 
-        final String next_activity = getIntent().getStringExtra("next_activity");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if( toolbar != null ) {
+            setSupportActionBar(toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("Enter Vehicle Number");
+            }
+        }
+
+        //final String next_activity = getIntent().getStringExtra("next_activity");
 
         final EditText regno = (EditText) findViewById(R.id.regno);
 
@@ -23,16 +38,8 @@ public class EnterRegNoActivity extends Activity {
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = null;
-                if ("ViewRC".equals(next_activity)) {
-                    intent = new Intent(EnterRegNoActivity.this, ViewRCActivity.class);
-                } else if ("ViewPUCC".equals(next_activity)) {
-                    intent = new Intent(EnterRegNoActivity.this, ViewPUCCActivity.class);
-                } else if ("ViewINS".equals(next_activity)) {
-                    intent = new Intent(EnterRegNoActivity.this, ViewINSActivity.class);
-                } else {
-                    intent = new Intent(EnterRegNoActivity.this, DisplayVehicleDetailsActivity.class);
-                }
+                hideSoftKeyboard(EnterRegNoActivity.this);
+                Intent intent = new Intent(EnterRegNoActivity.this, DisplayVehicleDetailsActivity.class);
                 intent.putExtra("vehicle_number",regno.getText().toString().trim());
                 startActivity(intent);
                 finish();
@@ -40,5 +47,13 @@ public class EnterRegNoActivity extends Activity {
         });
 
 
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
     }
 }
