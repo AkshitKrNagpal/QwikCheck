7340f6e9a55953fb4312ff23febeb177a19a82c2
akshitkrnagpal <akshitkrnagpal@gmail.com>
2017-04-17 06:10:33 +0530

    Modified ViewPUCCActivity

--- app/src/main/java/cf/qwikcheck/qwikcheck/ViewPUCCActivity.java
+++ app/src/main/java/cf/qwikcheck/qwikcheck/ViewPUCCActivity.java
@@ -5,7 +5,11 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -19,21 +23,37 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
+import cf.qwikcheck.qwikcheck.base.QwikCheckBaseActivity;
 import cf.qwikcheck.qwikcheck.helper.SessionHelper;
 import cf.qwikcheck.qwikcheck.utils.Constants;
 
-public class ViewPUCCActivity extends Activity {
+public class ViewPUCCActivity extends QwikCheckBaseActivity {
+
+    private LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_pucc);
 
-        final TextView pucc_details = (TextView) findViewById(R.id.pucc_details);
+        //final TextView pucc_details = (TextView) findViewById(R.id.pucc_details);
 
         final String vehicle_id = getIntent().getStringExtra("vehicle_number");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if( toolbar != null ) {
+            setSupportActionBar(toolbar);
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setTitle("PUCC Details : "+vehicle_id);
+            }
+        }
+
+        container = (LinearLayout) findViewById(R.id.pucc_container);
+
 
         final ProgressDialog LoadingDialog = ProgressDialog.show(this, "Loading", "Please wait...", true);
 
@@ -68,7 +88,12 @@ public class ViewPUCCActivity extends Activity {
 
                                 JSONObject details = jsonObject.getJSONObject("details");
 
-                                pucc_details.setText(details.toString());
+                                Iterator<String> iter = details.keys();
+
+                                while( iter.hasNext() ) {
+                                    String key = iter.next();
+                                    addToContainer(key,details.getString(key));
+                                }
 
                             }
 
@@ -109,5 +134,30 @@ public class ViewPUCCActivity extends Activity {
             }
         };
         queue.add(postRequest);
+    }
+
+    public void addToContainer(String key,String value) {
+
+        LinearLayout row = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0,8,0,8);
+        row.setLayoutParams(layoutParams);
+
+
+        TextView key_view = new TextView(this);
+        LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT,2);
+        key_view.setLayoutParams(layoutParams1);
+        key_view.setText(key);
+
+        TextView value_view = new TextView(this);
+        LinearLayout.LayoutParams layoutParams2 = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT,3);
+        value_view.setLayoutParams(layoutParams2);
+        value_view.setText(value);
+
+        row.addView(key_view);
+        row.addView(value_view);
+
+        container.addView(row);
+
     }
 }
